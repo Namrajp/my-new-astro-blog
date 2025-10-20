dc6b1474d88e9ee20619a772e4b7efd17b49ecb2
namrajp <active99raj@gmail.com>
2025-10-20 19:39:20 +1100

    Add: logo and nav styles

---
+++ src/pages/main.ts
@@ -0,0 +1,24 @@
+function getNavbarHeight() {
+  const navToggle = document.querySelector<HTMLElement>(".nav-toggle");
+  const links = document.querySelector<HTMLElement>(".nav-links");
+  const linksContainer =
+    document.querySelector<HTMLElement>(".links-container");
+
+  if (!navToggle || !links || !linksContainer) {
+    console.warn("Navbar elements not found");
+    return;
+  }
+  navToggle.addEventListener("click", function () {
+    //   links.classList.toggle("show-links");
+    //   console.log(linksHeight);
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    //   console.log(containerHeight);
+    if (containerHeight === 0) {
+      linksContainer.style.height = `${linksHeight}px`;
+    } else {
+      linksContainer.style.height = "0px";
+    }
+  });
+}
+getNavbarHeight();
